f8c1e21c9d3d1da25d86cc74f5c544b2c5bcc99c
jwabo <joel@danihelgroup.com>
2022-10-12 10:05:20 -0600

    First commit

--- test/integration_test/app_test.dart
+++ test/integration_test/app_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:carte_crm_test/main_dev.dart' as app;
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('Test carter app', (tester) async {
