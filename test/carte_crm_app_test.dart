b2b5a5b3fb4afdb3179da1b661900ba1b623effa
jwabo <joel@danihelgroup.com>
2022-10-12 10:19:21 -0600

    First commit

---
+++ test/carte_crm_app_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Carter crm app', (WidgetTester tester) async {
+    /* await tester.pumpWidget(const Root());
+
+    expect(find.text('January'), findsAtLeastNWidgets(1));
+    expect(find.text('6'), findsNothing);*/
+
+  });
+}
